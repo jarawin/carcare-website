cb0cf4a0891fb71e46a17b1b15d1d3bd8a0c9297
Pitachios <nattorikung@gmail.com>
2022-07-08 20:53:50 +0700

    th

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
           onClick={() => { handleChangeLanguage() }}>
-          EEIihgufy
+          th
         </a>
       </header>
     </div>
